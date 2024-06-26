a4f4674f7d54fb3a70dfc17c623c18657f77095f
Ajharali-Shaikh15039 <ajharalishaikh@gmail.com>
2024-06-27 00:54:43 +0200

    Updated data_pipeline.py

--- project/data_pipeline.py
+++ project/data_pipeline.py
@@ -58,15 +58,14 @@ def merge_data(accidents_df, weather_df):
     return df
 
 
-def save_to_sqlite(data_df, table_name, db_path='./data/processed_database.sqlite'):
+def save_to_sqlite(data_df, table_name, db_path='../data/processed_database.sqlite'):
     os.makedirs(os.path.dirname(db_path), exist_ok=True)
     with sqlite3.connect(db_path) as conn:
         data_df.to_sql(table_name, conn, index=False, if_exists='replace')
 
 
 def main():
-    accidents = load_accident_df('./data/accident_data.csv')
-    # accidents = load_accident_df('../data/accident_data.csv')
+    accidents = load_accident_df('../data/accident_data.csv')
     weather_df = load_weather_df('https://opendata.dwd.de/climate_environment/CDC/regional_averages_DE/monthly')
     drop_columns = ['LAND', 'LOR', 'UJAHR', 'XGCSWGS84', 'YGCSWGS84', 'BEZ', 'OBJECTID', 'STRASSE',
                     'USTUNDE', 'UWOCHENTAG', 'UKATEGORIE', 'UART', 'UTYP1', 'ULICHTVERH', 'IstSonstige','LINREFX','LINREFY']
@@ -87,7 +86,7 @@ def main():
     accidents_df = preprocess_accidents(accidents, drop_columns, columns_rename, months_rename, road_conditions_rename, 'Hellersdorfer Promenade')
     final_df = merge_data(accidents_df, weather_df)
     save_to_sqlite(final_df, 'final_data_table')
-    print('Complete!')
+
 
 if __name__ == "__main__":
     main()
